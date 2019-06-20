5d677ea30ad9402ad8df48236f08b25b0fb6a992
Yaroslav882 <nagniy_ya@yahoo.com>
2019-06-20 17:44:27 +0300

    Changed the code

--- simple_bot.py
+++ simple_bot.py
@@ -1,14 +1,23 @@
 import telebot
+import parser
 
-simple_bot = telebot.TeleBot('740731369:AAF574uUdnizwHGgnB8L0I6Pjl7mht-NOok')
+TOKEN = '869498520:AAE-JUP9a4AJXW9AWzD_M2WLZOZKc3rKaDE'
+simple_bot = telebot.TeleBot(TOKEN)
+
+@simple_bot.message_handler(commands=['start', 'go'])
+def srat_bot(message):
+    simple_bot.send_message(message.chat.id, 'Добро пожаловать')
+    simple_bot.send_message(message.chat.id, 'Я бот который парсит с Habr.com')
 
 @simple_bot.message_handler(content_types=['text'])
-def get_messages(message):
-    if message.text == 'Привет!':
-        simple_bot.send_message(message.from_user.id, 'Привет, что тебе надо?')
-    elif message.text == '/help':
-        simple_bot.send_message(message.from_user.id, 'Напиши "Привет!"')
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == 'привет':
+        simple_bot.send_message(chat_id, 'И тебе салют, я парсерный бот.')
+    elif text == 'что делаешь?':
+        simple_bot.send_message(chat_id, 'Пытаюсь выйти в интернет!')
     else:
-        simple_bot.send_message(message.from_user.id, 'Непонятно! Напиши /help')
+        simple_bot.send_message(chat_id, 'Я что то не понял твой текст.')
 
-simple_bot.polling(none_stop=True, interval=0)
+simple_bot.polling()
